68ac968aeb4cd5cd12218fe88ee60dd6110f9086
Florencia <flordani98@gmail.com>
2024-07-01 19:43:43 -0300

    Se maquetó y estilizó la página error404

--- src/app/standalones/error404/error404.component.ts
+++ src/app/standalones/error404/error404.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'shared-error404',
   templateUrl: './error404.component.html',
+  styleUrls: ['./error404.component.css'],
 
   standalone: true
 })
